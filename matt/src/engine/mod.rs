1944441afa8ff40ad6e82b5c01db80b8a9c9018f
Gustavo Ramos Rehermann <rehermann6046@gmail.com>
2022-12-26 21:01:10 -0300

    Fix matt/src/engine preludes in refactors

--- matt/src/engine/mod.rs
+++ matt/src/engine/mod.rs
@@ -1,12 +1,8 @@
-pub mod common;
-pub mod data;
 pub mod engine;
 pub mod midi;
 pub mod render;
 
 pub mod prelude {
-    pub use super::common::prelude::*;
-    pub use super::data::prelude::*;
     pub use super::engine::prelude::*;
     pub use super::midi::prelude::*;
     pub use super::render::prelude::*;
---
+++ matt/src/ir/lib.rs
@@ -0,0 +1,5 @@
+pub mod data;
+
+pub mod prelude {
+    pub use data::prelude::*;
+}
